1926868159ccae3d27d2863d408c631b720c82ba
karimovdostonbek1992-commits <karimovdostonbek1992@gmail.com>
2026-02-07 19:45:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,7 @@
-npm install
-npm run build
+import { defineConfig } from 'vite'
+import react from '@vitejs/react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // BU JUDA MUHIM: GitHub Pages fayllarni topishi uchun kerak
+})
