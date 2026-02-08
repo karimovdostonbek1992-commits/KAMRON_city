8596323ac432b166269656560c92f1e5f5f6d75c
karimovdostonbek1992-commits <karimovdostonbek1992@gmail.com>
2026-02-08 10:11:02 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './', // BU JUDA MUHIM: GitHub Pages fayllarni topishi uchun kerak
-})
